92157ae326a38dd1aadd7e7efbbae5a5916bfaf6
Andrew Hoefling <andrewh@fileonq.com>
2021-06-27 00:11:49 -0400

    myfiles page

---
+++ src/UnoDrive.Shared/ViewModels/MyFilesViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using UnoDrive.Models;
+
+namespace UnoDrive.ViewModels
+{
+    public class MyFilesViewModel
+    {
+        public MyFilesViewModel()
+        {
+            FilesAndFolders = new ObservableCollection<ExplorerItem>(new[]
+            {
+                new ExplorerItem
+                {
+                    Name = "Test",
+                    FileSize = "100MB",
+                    Modified = DateTime.Now,
+                    ModifiedBy = "Andrew",
+                    Sharing = ""
+                }
+            });
+        }
+
+        public ObservableCollection<ExplorerItem> FilesAndFolders { get; set; }
+    }
+}
