4b4b0008b95248d1bad872bf412821d65d8b8c08
Andrew Hoefling <andrewh@fileonq.com>
2021-07-03 19:27:54 -0400

    fixed android on gridview

--- src/UWP/UnoDrive.Shared/Services/NavigationService.cs
+++ src/UWP/UnoDrive.Shared/Services/NavigationService.cs
@@ -1,21 +1,40 @@
-﻿using UnoDrive.Views;
+﻿using System.Threading.Tasks;
+using UnoDrive.Views;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using UnoDrive.Authentication;
 
 namespace UnoDrive.Services
 {
     public interface INavigationService
     {
         void NavigateToDashboard();
+        Task SignOutAsync();
     }
 
     public class NavigationService : INavigationService
     {
+        readonly IAuthenticationService authenticationService;
+
+        public NavigationService(IAuthenticationService authenticationService)
+        {
+            this.authenticationService = authenticationService;
+        }
+
         public void NavigateToDashboard()
         {
             if (Window.Current.Content is Frame rootFrame)
             {
                 rootFrame.Navigate(typeof(Dashboard), null);
+            }
+        }
+
+        public async Task SignOutAsync()
+        {
+            if (Window.Current.Content is Frame rootFrame)
+            {
+                rootFrame.Navigate(typeof(LoginPage), null);
+                await authenticationService.SignOutAsync();
             }
         }
     }
--- src/UWP/UnoDrive.Shared/Views/Dashboard.xaml.cs
+++ src/UWP/UnoDrive.Shared/Views/Dashboard.xaml.cs
@@ -1,6 +1,8 @@
 using System;
+using UnoDrive.Services;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using Uno.Extensions;
 
 namespace UnoDrive.Views
 {
@@ -16,23 +18,28 @@ namespace UnoDrive.Views
             //contentFrame.NavigateToType(typeof(MyFilesPage), null);
         }
 
-        void MenuItemSelected(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        async void MenuItemSelected(NavigationView sender, NavigationViewItemInvokedEventArgs args)
         {
-        //    var navOptions = new FrameNavigationOptions();
-        //    navOptions.TransitionInfoOverride = args.RecommendedNavigationTransitionInfo;
+            //    var navOptions = new FrameNavigationOptions();
+            //    navOptions.TransitionInfoOverride = args.RecommendedNavigationTransitionInfo;
 
-        //    Type pageType = default;
-        //    if (myFiles == args.InvokedItemContainer)
-        //        pageType = typeof(MyFilesPage);
-        //    else if (recentFiles == args.InvokedItemContainer)
-        //        pageType = typeof(RecentFilesPages);
-        //    else if (sharedFiles == args.InvokedItemContainer)
-        //        pageType = typeof(SharedFilesPage);
-        //    else if (recycleBin == args.InvokedItemContainer)
-        //        pageType = typeof(RecycleBinPage);
+            //    Type pageType = default;
+            //    if (myFiles == args.InvokedItemContainer)
+            //        pageType = typeof(MyFilesPage);
+            //    else if (recentFiles == args.InvokedItemContainer)
+            //        pageType = typeof(RecentFilesPages);
+            //    else if (sharedFiles == args.InvokedItemContainer)
+            //        pageType = typeof(SharedFilesPage);
+            //    else if (recycleBin == args.InvokedItemContainer)
+            //        pageType = typeof(RecycleBinPage);
 
-        //    contentFrame.NavigateToType(pageType, null, navOptions);
+            //    contentFrame.NavigateToType(pageType, null, navOptions);
 
+            if (signOut == args.InvokedItemContainer)
+            {
+                var nav = (INavigationService)((App)App.Current).Container.GetService(typeof(INavigationService));
+                await nav.SignOutAsync();
+            }
         }
     }
 }
