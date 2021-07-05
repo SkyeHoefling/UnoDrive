e49766d0da2957c2191929e4c203da9d5c476686
Andrew Hoefling <andrewh@fileonq.com>
2021-07-04 23:37:51 -0400

    Removed stale property 'path'

--- src/UnoDrive.Shared/Models/Location.cs
+++ src/UnoDrive.Shared/Models/Location.cs
@@ -3,7 +3,6 @@
 	public class Location
 	{
 		public string Id { get; set; }
-		public string Path { get; set; }
 		public bool CanMoveBack => Back != null;
 		public bool CanMoveForward => Forward != null;
 		public Location Back { get; set; }
