6e54d3f295962a75ccf8f1e6ce3e669243516c65
Skye Hoefling <17751436+SkyeHoefling@users.noreply.github.com>
2022-07-06 15:37:58 -0400

    Added loginpage and VM hooked up button

---
+++ src/UWP/UnoDrive.Shared/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace UnoDrive.ViewModels
+{
+    public class LoginViewModel
+    {
+        public LoginViewModel()
+        {
+            Login = new RelayCommand(OnLogin);
+        }
+
+        public ICommand Login { get; }
+
+        void OnLogin()
+        {
+            System.Console.WriteLine("Perform login");
+        }
+    }
+}
