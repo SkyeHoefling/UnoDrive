00c9e3586908a1a548e05d85fe440027684d1e4d
Skye Hoefling <17751436+SkyeHoefling@users.noreply.github.com>
2022-07-06 15:39:03 -0400

    Chapter 4 - Updated to winui3 + NET 6

--- Chapter 4/UnoDrive.Shared/LoginPage.xaml.cs
+++ Chapter 4/UnoDrive.Shared/LoginPage.xaml.cs
@@ -1,28 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+﻿using Microsoft.UI.Xaml.Controls;
 
 namespace UnoDrive
 {
-    public sealed partial class LoginPage : Page
+	public sealed partial class LoginPage : Page
     {
 		public LoginPage()
 		{
 			this.InitializeComponent();
-#if NETFX_CORE
+#if NET6_0_OR_GREATER && WINDOWS
 			header.Text = "Hello from UWP";
 #elif __ANDROID__
 			header.Text = "Hello from Android";
