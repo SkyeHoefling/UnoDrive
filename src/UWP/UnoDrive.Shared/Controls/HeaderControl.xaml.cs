a933b30af8f7a0f332a6f2c2a3a5c73766435a8d
Skye Hoefling <17751436+SkyeHoefling@users.noreply.github.com>
2022-07-06 15:37:57 -0400

    Copied winui shared code over to uwp project

---
+++ src/UWP/UnoDrive.Shared/Controls/HeaderControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace UnoDrive.Controls
+{
+    public sealed partial class HeaderControl : UserControl
+    {
+        public HeaderControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
