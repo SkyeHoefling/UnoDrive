cc914878c49368c82c69f46d4487b4937bffaa5c
Skye Hoefling <17751436+SkyeHoefling@users.noreply.github.com>
2022-07-06 15:38:34 -0400

    Updated AuthenticationService to use networkConnecitvityService instead of Xamarin Essentials

--- src/UnoDrive.Shared/Authentication/AuthenticationService.cs
+++ src/UnoDrive.Shared/Authentication/AuthenticationService.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Uno.UI.MSAL;
+using UnoDrive.Services;
+using Windows.Networking.Connectivity;
 using Xamarin.Essentials;
 
 using MsalAuthenticationResult = Microsoft.Identity.Client.AuthenticationResult;
@@ -22,16 +24,22 @@ namespace UnoDrive.Authentication
 
 	public class AuthenticationService : IAuthenticationService
 	{
-		private string[] _scopes;
-		protected MsalIPublicClientApplication PublicClientApp { get; }
-		protected ILogger Logger { get; }
+		string[] scopes;
+		
+		readonly MsalIPublicClientApplication publicClientApp;
+		readonly INetworkConnectivityService networkService;
+		readonly ILogger logger;
+		
 		public AuthenticationService(
 			MsalIPublicClientApplication publicClientApp,
+			INetworkConnectivityService networkService,
 			ILogger<AuthenticationService> logger)
 		{
-			PublicClientApp = publicClientApp;
-			Logger = logger;
-			_scopes = new[] { "email", "Files.ReadWrite.All", "offline_access", "profile", "User.Read" };
+			this.publicClientApp = publicClientApp;
+			this.networkService = networkService;
+			this.logger = logger;
+
+			scopes = new[] { "email", "Files.ReadWrite.All", "offline_access", "profile", "User.Read" };
 		}
 
 		public async Task<IAuthenticationResult> AcquireTokenAsync()
@@ -50,33 +58,33 @@ namespace UnoDrive.Authentication
 
 			try
 			{
-				Logger.LogInformation("Attempting to perform interactive sign in");
-				Logger.LogInformation($"Authentication Scopes: {JsonConvert.SerializeObject(_scopes)}");
+				logger.LogInformation("Attempting to perform interactive sign in");
+				logger.LogInformation($"Authentication Scopes: {JsonConvert.SerializeObject(scopes)}");
 
-				authResult = await PublicClientApp
-					.AcquireTokenInteractive(_scopes)
+				authResult = await publicClientApp
+					.AcquireTokenInteractive(scopes)
 					.WithUnoHelpers()
 					.ExecuteAsync();
 			}
 			catch (MsalException ex)
 			{
-				Logger.LogError(ex, ex.Message);
+				logger.LogError(ex, ex.Message);
 				message = ex.Message;
 			}
 			catch (Exception ex)
 			{
-				Logger.LogError(ex, ex.Message);
+				logger.LogError(ex, ex.Message);
 				message = "Unable to sign-in, try again";
 			}
 
 			if (authResult == null)
 			{
-				Logger.LogError("Unable to retrieve Access Token from Azure Active Directory; 'authResult' is null");
+				logger.LogError("Unable to retrieve Access Token from Azure Active Directory; 'authResult' is null");
 
 				// This throws an exception on WPF
 				// Xamarin Essentials doesn't support all the target heads
-				if (Connectivity.NetworkAccess != NetworkAccess.Internet)
-					Logger.LogCritical("NO INTERNET CONNECTION: Internet required to retrieve an Access Token for the first time");
+				if (networkService.Connectivity != NetworkConnectivityLevel.InternetAccess)
+					logger.LogCritical("NO INTERNET CONNECTION: Internet required to retrieve an Access Token for the first time");
 			}
 
 			var result = authResult.ToAuthenticationResult(message, authResult?.Account?.HomeAccountId.ObjectId);
@@ -98,43 +106,43 @@ namespace UnoDrive.Authentication
 			string message = string.Empty;
 			MsalAuthenticationResult msalAuthResult = null;
 
-			var accounts = await PublicClientApp.GetAccountsAsync();
+			var accounts = await publicClientApp.GetAccountsAsync();
 			var firstAccount = accounts.FirstOrDefault();
 
 			if (firstAccount == null)
 			{
-				Logger.LogInformation("Unable to find Account in MSAL.NET Cache");
+				logger.LogInformation("Unable to find Account in MSAL.NET Cache");
 				return default(AuthenticationResult);
 			}
 
 			if (accounts.Any())
-				Logger.LogInformation($"Number of Accounts: {accounts.Count()}");
+				logger.LogInformation($"Number of Accounts: {accounts.Count()}");
 
 
 			try
 			{
-				Logger.LogInformation("Attempting to perform silent sign in . . .");
-				Logger.LogInformation($"Authentication Scopes: {JsonConvert.SerializeObject(_scopes)}");
-				Logger.LogInformation($"Account Name: {firstAccount.Username}");
+				logger.LogInformation("Attempting to perform silent sign in . . .");
+				logger.LogInformation($"Authentication Scopes: {JsonConvert.SerializeObject(scopes)}");
+				logger.LogInformation($"Account Name: {firstAccount.Username}");
 
-				msalAuthResult = await PublicClientApp
-					.AcquireTokenSilent(_scopes, firstAccount)
+				msalAuthResult = await publicClientApp
+					.AcquireTokenSilent(scopes, firstAccount)
 					.WithForceRefresh(false)
 					.ExecuteAsync();
 			}
 			catch (MsalUiRequiredException ex)
 			{
-				Logger.LogWarning(ex, ex.Message);
-				Logger.LogWarning("Unable to retrieve silent sign in Access Token");
+				logger.LogWarning(ex, ex.Message);
+				logger.LogWarning("Unable to retrieve silent sign in Access Token");
 				message = ex.Message;
 			}
 			catch (HttpRequestException ex)
 			{
-				if (Connectivity.NetworkAccess == NetworkAccess.Internet)
-					Logger.LogError(ex, ex.Message);
+				if (networkService.Connectivity == NetworkConnectivityLevel.InternetAccess)
+					logger.LogError(ex, ex.Message);
 				else if (firstAccount != null)
 				{
-					Logger.LogWarning($"Access Token expired, assuming user is {firstAccount.Username}");
+					logger.LogWarning($"Access Token expired, assuming user is {firstAccount.Username}");
 					var offlineResult = new OfflineAuthenticationResult
 					{
 						IsSuccess = true,
@@ -149,13 +157,13 @@ namespace UnoDrive.Authentication
 			}
 			catch (Exception ex)
 			{
-				Logger.LogWarning(ex, ex.Message);
-				Logger.LogWarning("Unable to retrieve silent sign in Access Token");
+				logger.LogWarning(ex, ex.Message);
+				logger.LogWarning("Unable to retrieve silent sign in Access Token");
 				message = ex.Message;
 			}
 
 			if (!string.IsNullOrEmpty(msalAuthResult?.AccessToken))
-				Logger.LogInformation("Successfully acquired Access Token from silent sign in");
+				logger.LogInformation("Successfully acquired Access Token from silent sign in");
 
 			var result = msalAuthResult.ToAuthenticationResult(message, firstAccount?.HomeAccountId?.ObjectId);
 
@@ -166,12 +174,12 @@ namespace UnoDrive.Authentication
 
 		public async Task SignOutAsync()
 		{
-			var accounts = await PublicClientApp.GetAccountsAsync();
+			var accounts = await publicClientApp.GetAccountsAsync();
 			var firstAccount = accounts.FirstOrDefault();
 			if (firstAccount != null)
 			{
-				await PublicClientApp.RemoveAsync(firstAccount);
-				Logger.LogInformation($"Removed account: {firstAccount.Username}, user succesfully logged out");
+				await publicClientApp.RemoveAsync(firstAccount);
+				logger.LogInformation($"Removed account: {firstAccount.Username}, user succesfully logged out");
 			}
 		}
 	}
