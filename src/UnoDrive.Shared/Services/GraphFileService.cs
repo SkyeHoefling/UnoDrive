f1f7ad8e2d15c7f8a2c14ff88ba1e941f79343a9
Skye Hoefling <17751436+SkyeHoefling@users.noreply.github.com>
2022-07-06 15:38:13 -0400

    Created new GraphFileService for connecting to graph; Updated VM code to use new service

---
+++ src/UnoDrive.Shared/Services/GraphFileService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using UnoDrive.Models;
+
+namespace UnoDrive.Services
+{
+	public interface IGraphFileService
+	{
+		Task<IEnumerable<OneDriveItem>> GetRootFiles();
+	}
+	
+	public class GraphFileService : IGraphFileService, IAuthenticationProvider
+	{
+		readonly GraphServiceClient graphClient;
+		public GraphFileService()
+		{
+#if __WASM__
+            var httpClient = new HttpClient(new Uno.UI.Wasm.WasmHttpHandler());
+#else
+			var httpClient = new HttpClient();
+#endif
+
+			graphClient = new GraphServiceClient(httpClient);
+			graphClient.AuthenticationProvider = this;
+		}
+		
+		public async Task<IEnumerable<OneDriveItem>> GetRootFiles()
+		{
+			var rootChildren = await graphClient.Me.Drive.Root.Children
+				.Request()
+				.GetAsync();
+
+			return rootChildren
+				.Select(driveItem => new OneDriveItem
+				{
+					Id = driveItem.Id,
+					Name = driveItem.Name,
+					FileSize = $"{driveItem.Size}",
+					Modified = driveItem.LastModifiedDateTime.HasValue ?
+						driveItem.LastModifiedDateTime.Value.LocalDateTime : DateTime.Now,
+					Type = driveItem.Folder != null ? OneDriveItemType.Folder : OneDriveItemType.File
+					//ModifiedBy = driveItem.LastModifiedByUser.DisplayName,
+					//Sharing = ""
+				});
+		}
+
+		public Task AuthenticateRequestAsync(HttpRequestMessage request)
+		{
+			var token = ((App)App.Current).AuthenticationResult?.AccessToken;
+			if (string.IsNullOrEmpty(token))
+				throw new System.Exception("No Access Token");
+
+			request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+			return Task.CompletedTask;
+		}
+	}
+}
--- src/UnoDrive.Shared/ViewModels/MyFilesViewModel.cs
+++ src/UnoDrive.Shared/ViewModels/MyFilesViewModel.cs
@@ -1,88 +1,49 @@
-﻿using System;
-using System.Linq;
-using System.Collections.ObjectModel;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using Microsoft.Graph;
+﻿using System.Linq;
 using UnoDrive.Models;
 using System.Collections.Generic;
 using Windows.UI.Xaml.Controls;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using UnoDrive.Services;
 
 namespace UnoDrive.ViewModels
 {
-    public class MyFilesViewModel : ObservableObject, IAuthenticationProvider
-    {
-        public MyFilesViewModel()
-        {
-            FilesAndFolders = new List<OneDriveItem>();
-            LoadData();
-        }
+	public class MyFilesViewModel : ObservableObject
+	{
+		readonly IGraphFileService graphFileService;
+		public MyFilesViewModel(IGraphFileService graphFileService)
+		{
+			this.graphFileService = graphFileService;
 
-        // We are not using an ObservableCollection
+			FilesAndFolders = new List<OneDriveItem>();
+			LoadData();
+		}
+
+		// We are not using an ObservableCollection
 		// by design. It can create significant performance
 		// problems and it wasn't loading correctly on Android.
-        List<OneDriveItem> filesAndFolders;
-        public List<OneDriveItem> FilesAndFolders
-        {
-            get => filesAndFolders;
-            set => SetProperty(ref filesAndFolders, value);
-        }
-        
-        public void ItemClick(object sender, ItemClickEventArgs args)
-        {
-	        if (args.ClickedItem is OneDriveItem driveItem)
-	        {
-		        if (driveItem.Type == OneDriveItemType.Folder)
-		        {
-			        // TODO - open folder
-		        }
-		        else
-		        {
-			        // TODO - open file
-		        }
-	        }
-        }
+		List<OneDriveItem> filesAndFolders;
+		public List<OneDriveItem> FilesAndFolders
+		{
+			get => filesAndFolders;
+			set => SetProperty(ref filesAndFolders, value);
+		}
 
-        async void LoadData()
-        {
-#if __WASM__
-            var httpClient = new HttpClient(new Uno.UI.Wasm.WasmHttpHandler());
-#else
-            var httpClient = new HttpClient();
-#endif
+		public void ItemClick(object sender, ItemClickEventArgs args)
+		{
+			if (args.ClickedItem is OneDriveItem driveItem)
+			{
+				if (driveItem.Type == OneDriveItemType.Folder)
+				{
+					// TODO - open folder
+				}
+				else
+				{
+					// TODO - open file
+				}
+			}
+		}
 
-            var graphClient = new GraphServiceClient(httpClient);
-            graphClient.AuthenticationProvider = this;
-
-            var rootChildren = await graphClient.Me.Drive.Root.Children
-                .Request()
-                .GetAsync();
-
-            FilesAndFolders = rootChildren
-                .Select(driveItem => new OneDriveItem
-                {
-	                Id = driveItem.Id,
-                    Name = driveItem.Name,
-                    FileSize = $"{driveItem.Size}",
-                    Modified = driveItem.LastModifiedDateTime.HasValue ?
-                        driveItem.LastModifiedDateTime.Value.LocalDateTime : DateTime.Now,
-                    Type = driveItem.Folder != null ? OneDriveItemType.Folder : OneDriveItemType.File
-                    //ModifiedBy = driveItem.LastModifiedByUser.DisplayName,
-                    //Sharing = ""
-                })
-                .ToList();
-        }
-
-        public Task AuthenticateRequestAsync(HttpRequestMessage request)
-        {
-            var token = ((App)App.Current).AuthenticationResult?.AccessToken;
-            if (string.IsNullOrEmpty(token))
-                throw new System.Exception("No Access Token");
-
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            return Task.CompletedTask;
-        }
-    }
+		async void LoadData() =>
+			FilesAndFolders = (await graphFileService.GetRootFiles()).ToList();
+	}
 }
