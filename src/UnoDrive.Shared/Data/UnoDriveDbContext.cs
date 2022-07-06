12dadcf278bd58cd167a328b6e8659fee1c3eb35
Skye Hoefling <17751436+SkyeHoefling@users.noreply.github.com>
2022-07-06 15:38:35 -0400

    Added notes for WASM ef core .net5 support

--- src/UnoDrive.Shared/Data/UnoDriveDbContext.cs
+++ src/UnoDrive.Shared/Data/UnoDriveDbContext.cs
@@ -18,6 +18,9 @@ namespace UnoDrive.Data
 		{
 			// TODO - Get correct path for each platform.
 			// Android - add system.data
+			// WASM - Follow Guide here https://github.com/unoplatform/Uno.SQLitePCLRaw.Wasm/blob/master/src/samples/EFCoreSample/EFCoreSample.Wasm/EFCoreSample.Wasm.csproj
+			//      Run WSL Command - bash -c `wslpath "C:\Users\AndrewHoefling\.nuget\packages\uno.wasm.bootstrap\2.1.0\build\scripts\dotnet-setup.sh"`
+			//      This is not supported in .net 5 - https://github.com/unoplatform/Uno.SQLitePCLRaw.Wasm/issues/19
 			var local = Windows.Storage.ApplicationData.Current.LocalFolder.Path;
 			var dbPath = Path.Combine(local, "unodrive.db");
 			optionsBuilder.UseSqlite($"data source={dbPath}");
