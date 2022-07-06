70d5baadd58f0ee6d2cc0168a33b3f54c4caad05
Skye Hoefling <17751436+SkyeHoefling@users.noreply.github.com>
2022-07-06 15:38:49 -0400

    Wired up navigation to dashboard

--- Chapter 6/UnoDrive.Shared/Views/Dashboard.xaml.cs
+++ Chapter 6/UnoDrive.Shared/Views/Dashboard.xaml.cs
@@ -13,23 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace UnoDrive.Views
 {
-    public sealed partial class LoginPage : Page
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Dashboard : Page
     {
-        public LoginPage()
+        public Dashboard()
         {
             this.InitializeComponent();
-		}
-
-		void OnLoginClick(object sender, RoutedEventArgs args)
-		{
-			if (Window.Current.Content is Frame rootFrame)
-			{
-				rootFrame.Navigate(typeof(Dashboard), null);
-			}
-		}
-	}
+        }
+    }
 }
