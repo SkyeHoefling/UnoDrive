8ecf08960fb027a97b3bfb90ec78e8ea282732b5
Skye Hoefling <17751436+SkyeHoefling@users.noreply.github.com>
2022-07-06 15:40:29 -0400

    Ch 15 - updated to latest nuget packages

--- Chapter 15/UnoDrive.Shared/Authentication/AuthenticationConfiguration.cs
+++ Chapter 15/UnoDrive.Shared/Authentication/AuthenticationConfiguration.cs
@@ -26,7 +26,7 @@ namespace UnoDrive.Authentication
 #endif
 
 #if __IOS__ || __MACOS__
-			builder.WithIosKeychainSecurityGroup("com.HoeflingSoftware.UnoWinUI");
+			builder.WithIosKeychainSecurityGroup("com.AndrewHoefling.UnoDrive");
 #endif
 
 			services.AddSingleton(builder.Build());
