e36ace9c997a83746e593288fb864df224de952e
Skye Hoefling <17751436+SkyeHoefling@users.noreply.github.com>
2022-07-06 15:38:03 -0400

    Fixed WASM impl

--- src/UWP/UnoDrive.Shared/Authentication/AuthenticationService.cs
+++ src/UWP/UnoDrive.Shared/Authentication/AuthenticationService.cs
@@ -3,7 +3,9 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Client;
 using Newtonsoft.Json;
+using Uno.UI.MSAL;
 using Xamarin.Essentials;
 
 using MsalAuthenticationResult = Microsoft.Identity.Client.AuthenticationResult;
@@ -46,15 +48,21 @@ namespace UnoDrive.Authentication
 
                 authResult = await PublicClientApp
                     .AcquireTokenInteractive(_scopes)
+                    .WithUnoHelpers()
                     .ExecuteAsync();
+
+                Console.WriteLine("Complete");
+                Console.WriteLine(authResult.AccessToken);
             }
             catch (MsalException ex)
             {
+                Console.WriteLine(ex);
                 Logger.LogError(ex, ex.Message);
                 message = ex.Message;
             }
             catch (Exception ex)
             {
+                Console.WriteLine(ex);
                 Logger.LogError(ex, ex.Message);
                 message = "Unable to sign-in, try again";
             }
